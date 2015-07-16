7932da72afcc0a87d8fa467c7eb4fbab937a65a2
josephhan11 <sjhan123@gmail.com>
2015-07-16 14:33:21 +0000

    some

--- application_controller.rb
+++ application_controller.rb
@@ -3,8 +3,15 @@ Bundler.require
 
 class MyApp < Sinatra::Base
 
-  get '/' do
-    erb :index
+  
+#  get '/' do
+#    erb :index
+#  end
+  post '/outfit' do
+    #stuff goes here
+    puts params
+    "Yayy I know your dog"
+    @outfit1 = Outfit.new(params[:zipcode], params[:sex], params[:occasion])
+    erb :outfit
   end
-
 end
