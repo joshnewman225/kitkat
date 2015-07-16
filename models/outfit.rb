f9aded12fb82eaaa32db3fdeec6dd1c4a3e1cdb3
josephhan11 <sjhan123@gmail.com>
2015-07-16 17:37:09 +0000

    close

--- models/outfit.rb
+++ models/outfit.rb
@@ -6,7 +6,7 @@ require_relative './occasions/night_out/nightout.rb'
 require_relative './occasions/office_work/officework.rb'
 
 class Outfit
-  attr_accessor :zipcode, :sex, :occasion, :temperature, :city, :region, :country, :condition, :temperature
+  attr_accessor :zipcode, :sex, :occasion, :temperature, :city, :region, :country, :condition, :temperature, :final_outfit
   
   def initialize(zipcode, sex, occasion)
     @zipcode = zipcode
@@ -20,6 +20,20 @@ class Outfit
     @condition = xml.elements['//yweather:condition'].attributes['text']
     @temperature = xml.elements['//yweather:condition'].attributes['temp']
     @degrees = xml.elements['//yweather:units'].attributes['temperature']
+    
+    if occasion == 'casual'
+      outfit1 = Casual.new
+    elsif occasion == 'night out'
+      outfit1 = Night_Out.new
+#    elsif occasion == 'office work'
+#      outfit1 = 
+#    else
+    end
+    outfit1.outfits.each {|specific, information|
+      if (sex==info.values_at("gender"))&&(@temperature>=info.values_at("temp_low"))&&(@temperature<=info.values_at("temp_high"))
+        @final_outfit = information
+      end
+      }
   end
 end
 
