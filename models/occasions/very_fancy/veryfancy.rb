e705256559bc6c899c2d2f6bc148659b9a84fbc3
beatricekarp <coolbeatrice@aol.com>
2015-07-17 14:00:42 +0000

    almost

--- models/occasions/very_fancy/veryfancy.rb
+++ models/occasions/very_fancy/veryfancy.rb
@@ -7,7 +7,7 @@ class Very Fancy
       :gender => "female",
       :temp_low => "0",
       :temp_high => "120",
-      :image => "images/veryfancy_women_1.jpeg",
+      :image => "http://www.modaruzgari.org/wp-content/uploads/2014/02/98ab1507318fa6744bf612fe83cb9d15.jpg",
       :outfit => ["gold textured dress long sleeved, tan high heels,"]
       },
     :outfit2 => {
@@ -15,14 +15,14 @@ class Very Fancy
       :gender => "female",
       :temp_low => "0",
       :temp_high => "120",
-      :image => "images/veryfancy_women_2.jpg",
+      :image => "http://www.queeniedress.com/images/product/2015-short-chiffon-sheath-column-one-shoulder-sleeveless-homecoming-dress-4848-6.jpg",
       :outfit => ["light punk layered mini dress", "gold metal thick bracelet", "gold small hoop earrings", "tan high heels"],
     :outfit3 => {
       :name => "veryfancymen1",
       :gender => "male",
       :temp_low => "0",
       :temp_high => "120",
-      :image => "images/veryfancy_men_1.jpg",
+      :image => "http://img1.targetimg1.com/wcsstore/TargetSAS//img/p/14/54/14541337_Alt02_130511143000.jpg",
       :outfit => ["tuxedo suit", "black dress shoes", "black bow tie"]
       },
        :outfit4 => {
@@ -30,7 +30,7 @@ class Very Fancy
       :gender => "male",
       :temp_low => "0",
       :temp_high => "120",
-         :image => "images/veryfancy_men_2.jpg",
+         :image => "http://www.bachrachoutlet.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/0/9/0991-80523-GREY.jpg",
          :outfit => ["grey suit", "colorful tie", "brown dress shoes",]
       },
       
